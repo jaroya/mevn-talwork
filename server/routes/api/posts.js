ef11cecbe3f422ad5cb9448dbdb5404bd783cbce
jaroya <michael.jaroya@gmail.com>
2020-05-21 13:56:23 +0300

    Backend Complete

---
+++ server/routes/api/posts.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get Posts
+router.get('/', async (req,res)=>{
+
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
+
+});
+
+
+// Add Posts
+
+
+router.post('/', async(req,res)=>{
+
+	const posts = await loadPostsCollection();
+	await posts.insertOne({
+		text: req.body.text,
+		createdAt: new Date()
+	});
+
+	res.status(201).send();
+
+});
+
+
+// Delete Posts
+
+router.delete('/:id', async (req,res)=>{
+
+	const posts = await loadPostsCollection();
+	await posts.deleteOne({
+		_id: new mongodb.ObjectID(req.params.id)
+	});
+
+	res.status(200).send();
+})
+
+async function loadPostsCollection(){
+	const client = await mongodb.MongoClient.connect('mongodb://micah-mevn:realmenARE1234@ds151247.mlab.com:51247/mevn-talwork',{
+		useNewUrlParser: true
+	});
+
+	return client.db('mevn-talwork').collection('posts');
+}
+
+
+
+
+module.exports = router;
